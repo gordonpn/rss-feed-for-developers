e620686dc8bfcfa19459c90845a44d56ea095c3a
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-07-12 22:03:25 -0400

    chore: add healthcheck endpoint

--- backend/app.go
+++ backend/app.go
@@ -31,8 +31,34 @@ func (a *App) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, a.Router))
 }
 
+func (a *App) handleFeed() http.HandlerFunc {
+	log.Debug("Feed endpoint registered")
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
+
+func (a *App) handleHealthCheck() http.HandlerFunc {
+	log.Debug("Healthcheck API endpoint registered")
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Debug("Healthcheck pinged")
+		if err := a.DB.Ping(); err != nil {
+			respondWithJSON(w, http.StatusInternalServerError, map[string]string{"message": "not ok"})
+			log.Error(err.Error())
+			return
+		}
+		respondWithJSON(w, http.StatusOK, map[string]string{"message": "ok"})
+	}
+}
+
+func (a *App) handlePosts() http.HandlerFunc {
+	log.Debug("Posts API endpoint registered")
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
+
 func (a *App) initializeRoutes() {
 	a.Router.HandleFunc("/feed", a.handleFeed()).Methods("GET")
 	apiRoute := a.Router.PathPrefix("/api/v1").Subrouter()
 	apiRoute.HandleFunc("/healthcheck", a.handleHealthCheck()).Methods("GET")
+	apiRoute.HandleFunc("/posts", a.handlePosts()).Methods("GET")
 }
